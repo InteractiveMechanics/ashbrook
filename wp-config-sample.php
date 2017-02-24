c14d33c312f45818b57cbfd83150eda69bb76f6d
Christina Deemer <cahdeemer@gmail.com>
2017-02-24 14:11:50 -0500

    Adjust config files

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -79,6 +79,9 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', false);
 
+define('WP_HOME','http://localhost');
+define('WP_SITEURL','http://localhost');
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
