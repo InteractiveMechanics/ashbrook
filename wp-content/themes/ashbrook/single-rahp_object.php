830deea5c23619ef55b8807339f1290dbc143fc6
Christina Deemer <cahdeemer@gmail.com>
2017-03-10 17:38:07 -0500

    single object page update

--- wp-content/themes/ashbrook/single-rahp_object.php
+++ wp-content/themes/ashbrook/single-rahp_object.php
@@ -47,44 +47,246 @@
 	    		</ul>
 	    	</div>
 
-	    	<div class="container-fluid">
-	    		<div class="rahp-object-body col-sm-12">
-	    			<p>Gentlemen:</p>
+	    	<div class="container-fluid rahp-object-body">
+	    		<div class="row">
+	    			<div class="col-sm-12">
 
-					<p>While I received with much satisfaction your address replete with expressions of esteem, I rejoice in the opportunity of assuring you that I shall always retain grateful remembrance of the cordial welcome I experienced on my visit to Newport from all classes of citizens.</p>
+		    			<figure class="col-sm-4">
+		    				<div>
+		    				<img src="<?php printThemePath(); ?>/img/Capitol_Prayer_Room_stained_glass_window.jpg"> 
+		    				</div>
+		    				<figcaption>Capitol Prayer Room stained-glass window. <i>Suspendisse Vitae Risus Ipsum.</i> Etiam a tincidunt magna.</figcaption>
+		    			</figure>
+		    			<p>Gentlemen:</p>
 
-					<p>The reflection on the days of difficulty and danger which are past is rendered the more sweet from a consciousness that they are succeeded by days of uncommon prosperity and security.</p>
+						<p>While I received with much satisfaction your address replete with expressions of esteem, I rejoice in the opportunity of assuring you that I shall always retain grateful remembrance of the cordial welcome I experienced on my visit to Newport from all classes of citizens.</p>
 
-					<p>If we have wisdom to make the best use of the advantages with which we are now favored, we cannot fail, under the just administration of a good government, to become a great and happy people.</p>
+						<p>The reflection on the days of difficulty and danger which are past is rendered the more sweet from a consciousness that they are succeeded by days of uncommon prosperity and security.</p>
 
-					<p>The citizens of the United States of America have a right to applaud themselves for having given to mankind examples of an enlarged and liberal policy — a policy worthy of imitation. All possess alike liberty of conscience and immunities of citizenship.</p>
+						<p>If we have wisdom to make the best use of the advantages with which we are now favored, we cannot fail, under the just administration of a good government, to become a great and happy people.</p>
 
-					<p>It is now no more that toleration is spoken of as if it were the indulgence of one class of people that another enjoyed the exercise of their inherent natural rights, for, happily, the Government of the United States, which gives to bigotry no sanction, to persecution no assistance, requires only that they who live under its protection should demean themselves as good citizens in giving it on all occasions their effectual support.</p>
+						
 
-					<p>It would be inconsistent with the frankness of my character not to avow that I am pleased with your favorable opinion of my administration and fervent wishes for my felicity.</p>
 
-					<p>May the children of the stock of Abraham who dwell in this land continue to merit and enjoy the good will of the other inhabitants — while every one shall sit in safety under his own vine and fig tree and there shall be none to make him afraid.</p>
+						<p>The citizens of the United States of America have a right to applaud themselves for having given to mankind examples of an enlarged and liberal policy — a policy worthy of imitation. All possess alike liberty of conscience and immunities of citizenship.</p>
 
-					<p>May the father of all mercies scatter light, and not darkness, upon our paths, and make us all in our several vocations useful here, and in His own due time and way everlastingly happy.</p>
+						<p>It is now no more that toleration is spoken of as if it were the indulgence of one class of people that another enjoyed the exercise of their inherent natural rights, for, happily, the Government of the United States, which gives to bigotry no sanction, to persecution no assistance, requires only that they who live under its protection should demean themselves as good citizens in giving it on all occasions their effectual support.</p>
 
-					<p>G. Washington</p>
 
-					<div class="object-callouts col-sm-4">
-						<div class="callout-study-questions">
-							<h2>Study Questions</h2>
-							<h3>View All</h3>
-							<p>What is the difference between toleration and religious freedom? Would tolerance be acceptable given the argument of the Declaration of Independence about the rights that men are endowed with?</p>
-						</div>
-						<div class="callout-compare">
-							<h2>Compare</h2>
-							<p>How is Washington's view in his letter to the Hebrew Congregation similar to and different fromm the views expressed in these <a href="">excerpts of Colonial Laws</a>?</p>
-						</div>
-					</div>
-	  		  </div>
+						<p>It would be inconsistent with the frankness of my character not to avow that I am pleased with your favorable opinion of my administration and fervent wishes for my felicity.</p>
+
+						<p>May the children of the stock of Abraham who dwell in this land continue to merit and enjoy the good will of the other inhabitants — while every one shall sit in safety under his own vine and fig tree and there shall be none to make him afraid.</p>
 
 				
-	   
+
+						<p>May the father of all mercies scatter light, and not darkness, upon our paths, and make us all in our several vocations useful here, and in His own due time and way everlastingly happy.</p>
+
+						<p>G. Washington</p>
+
+					
+	  		  		</div>
+
+				</div>
+
+				<div class="row">
+					<div class="object-references col-sm-7">
+						<h2>Notes</h2>
+						<p>[1] Micah 4:4 ('But they shall sit every man under his vine and under his fig tree; and none shall make them afraid: for the mouth of the LORD of hosts has spoken it.') was one of the more popular Biblical passages during the Revolution. The preceding verse (Micah 3:4) is now better known: 'And he shall judge between many peoples, and rebuke strong nations afar off; and they shall beat their swords into plowshares, and their spears into pruninghooks: nation shall not lift up a sword against nation, neither shall they learn war any more.'</p>
+					</div>
+
+					<!-- THESE CALLOUTS ONLY FOR 25 CORE DOCS -->
+					<div class="object-callouts col-sm-5">
+							<div class="callout-studyqs">
+								<div class="studyqs-headings-wrapper">
+									<h2>Study Questions...</h2>
+									<h5><a href="">View All</a></h5>
+								</div>
+								<p>What is the difference between toleration and religious freedom? Would tolerance be acceptable given the argument of the Declaration of Independence about the rights that men are endowed with?</p>
+							</div>
+							<div class="callout-compare">
+								<h2>Compare...</h2>
+								<p>How is Washington's view in his letter to the Hebrew Congregation similar to and different fromm the views expressed in these <a href="">excerpts of Colonial Laws</a>?</p>
+							</div>
+					</div>
+					<!-- END CALLOUT SECTION-->
+
+					<div class="object-references col-sm-7">
+						<h2>Citation</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+					</div>
+
+					<div class="object-references col-sm-7">
+						<h2>External Links</h2>
+						<p><a href="">George Washington: A National Treasure - Smithsonian Institution</a></p>
+						<p><a href="">Mount Vernon - MountVernon.org</a></p>
+						<p><a href="">The Papers of George Washington - Library of Congress</a></p>
+
+					</div>
+
+				</div>
+
 	    	</div>
+
+	    	<div class="container-fluid related-content">
+	    		<div class="row">
+	    			<div class="col-sm-12">
+	    				<div class="columns-container">
+	    				<div class="columns-wrapper" data-columns="4">
+	    					<div class="single-column">
+	    						
+	    						<div class="relcon-single">
+	    							<a href="">
+	    								<img src="https://placekitten.com/200/340">
+	    								<div class="trapezoid">
+											<div class="trap-sq">
+												<h3>Source Object Title</h3>
+												<small>Source Author Name</small>
+												<small>Source Date</small>
+											</div>
+											<div class="trap-tri"></div>
+										</div>
+	    							</a>
+	    						</div>
+
+	    						<div class="relcon-single">
+	    							<a href="">
+	    								<img src="https://placekitten.com/200/340">
+	    								<div class="trapezoid">
+											<div class="trap-sq">
+												<h3>Source Object Title</h3>
+												<small>Source Author Name</small>
+												<small>Source Date</small>
+											</div>
+											<div class="trap-tri"></div>
+										</div>
+	    							</a>
+	    						</div>
+
+
+
+
+
+	    					</div>  <!-- end single-column -->
+
+
+	    					<div class="single-column">
+	    						
+	    						<div class="relcon-single">
+	    							<a href="">
+	    								<img src="https://placekitten.com/200/340">
+	    								<div class="trapezoid">
+											<div class="trap-sq">
+												<h3>Source Object Title</h3>
+												<small>Source Author Name</small>
+												<small>Source Date</small>
+											</div>
+											<div class="trap-tri"></div>
+										</div>
+	    							</a>
+	    						</div>
+
+	    						<div class="relcon-single">
+	    							<a href="">
+	    								<img src="https://placekitten.com/200/340">
+	    								<div class="trapezoid">
+											<div class="trap-sq">
+												<h3>Source Object Title</h3>
+												<small>Source Author Name</small>
+												<small>Source Date</small>
+											</div>
+											<div class="trap-tri"></div>
+										</div>
+	    							</a>
+	    						</div>
+
+
+
+
+
+	    					</div>  <!-- end single-column -->
+
+	    					<div class="single-column">
+	    						
+	    						<div class="relcon-single">
+	    							<a href="">
+	    								<img src="https://placekitten.com/200/340">
+	    								<div class="trapezoid">
+											<div class="trap-sq">
+												<h3>Source Object Title</h3>
+												<small>Source Author Name</small>
+												<small>Source Date</small>
+											</div>
+											<div class="trap-tri"></div>
+										</div>
+	    							</a>
+	    						</div>
+
+	    						<div class="relcon-single">
+	    							<a href="">
+	    								<img src="https://placekitten.com/200/340">
+	    								<div class="trapezoid">
+											<div class="trap-sq">
+												<h3>Source Object Title</h3>
+												<small>Source Author Name</small>
+												<small>Source Date</small>
+											</div>
+											<div class="trap-tri"></div>
+										</div>
+	    							</a>
+	    						</div>
+
+
+
+
+
+	    					</div>  <!-- end single-column -->
+
+
+	    					<div class="single-column">
+	    						
+	    						<div class="relcon-single">
+	    							<a href="">
+	    								<img src="https://placekitten.com/g/500/400">
+	    								<div class="trapezoid">
+											<div class="trap-sq">
+												<h3>Source Object Title</h3>
+												<small>Source Author Name</small>
+												<small>Source Date</small>
+											</div>
+											<div class="trap-tri"></div>
+										</div>
+	    							</a>
+	    						</div>
+
+	    						<div class="relcon-single">
+	    							<a href="">
+	    								<img src="https://placekitten.com/200/340">
+	    								<div class="trapezoid">
+											<div class="trap-sq">
+												<h3>Source Object Title</h3>
+												<small>Source Author Name</small>
+												<small>Source Date</small>
+											</div>
+											<div class="trap-tri"></div>
+										</div>
+	    							</a>
+	    						</div>
+
+
+
+
+
+	    					</div>  <!-- end single-column -->
+
+	    					</div>
+	    				</div>
+	    			</div>
+	    		</div>
+	    	</div>
+
+
+
+	    	
 
 	    	
 
