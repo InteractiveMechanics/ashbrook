caec6a56330b6b5a3454e59d866266592b46c1a9
Christina Deemer <cahdeemer@gmail.com>
2017-05-24 12:05:42 -0400

    testing map rest data on staging

--- wp-content/themes/ashbrook/js/map.js
+++ wp-content/themes/ashbrook/js/map.js
@@ -1,7 +1,36 @@
-var map = L.map('map', {zoomControl:true }).setView([39.967764, -75.229737], 17.5);
+var map = L.map('map', {zoomControl:true }).setView([38.014040, -93.938758], 4.35);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaW50ZXJhY3RpdmVtZWNoIiwiYSI6InJlcUtqSk0ifQ.RUwHuEkBbXoJ6SgOnXmYFg', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-    maxZoom: 18
+L.tileLayer('https://api.mapbox.com/styles/v1/interactivemech/cj30j5gyu00022smsa3qsecld/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaW50ZXJhY3RpdmVtZWNoIiwiYSI6InJlcUtqSk0ifQ.RUwHuEkBbXoJ6SgOnXmYFg', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// ADD DATA TO MAP
+
+var jsonData = { "type": "FeatureCollection", "features": [] };
+
+var dataSuccess = function(data) {
+	console.log(data);
+	$.each(data, function(index, value){
+		if (value.latitude && value.longitude){
+			var dataToAdd = {};
+			dataToAdd["type"] = "Feature";
+	        dataToAdd["geometry"] = {};
+	        dataToAdd["geometry"]["type"] = "Point";
+	        dataToAdd["geometry"]["coordinates"] = [value.longitude, value.latitude];
+	        dataToAdd["properties"] = {};
+	        dataToAdd["properties"]["ID"] = value.ID;
+	        dataToAdd["properties"]["title"] = value.post_title;
+	        dataToAdd["properties"]["location"] = value.location;
+
+	        jsonData["features"].push(dataToAdd);
+
+	    }
+
+	});
+
+};
+
+
+
+
+$.getJSON('http://localhost/wp-json/wp/v2/rahp_object', dataSuccess);
